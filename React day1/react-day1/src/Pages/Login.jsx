dbd37944bbcee679fbd7b0572e688bbc9dc7ae39
lakshayPS <lakshay.singh@cloudkeeper.com>
2025-11-19 20:11:01 +0530

    added username field

--- React day1/react-day1/src/Pages/Login.jsx
+++ React day1/react-day1/src/Pages/Login.jsx
@@ -1,7 +1,8 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 function Login({ onLogin, isAuthenticated }) {
+  const [username, setUsername] = useState("");
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -10,13 +11,27 @@ function Login({ onLogin, isAuthenticated }) {
     }
   }, [isAuthenticated, navigate]);
 
-  const handleLogin = () => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    localStorage.setItem("username", username);
     onLogin();
     navigate("/dashboard");
   };
   return (
     <div>
-      <button onClick={handleLogin}>Login</button>
+      <form onSubmit={handleSubmit}>
+        <div className="div">
+          <label htmlFor="username">Username</label>
+          <input
+            type="text"
+            id="username"
+            required
+            onChange={(e) => setUsername(e.target.value)}
+            placeholder="Enter username"
+          />
+        </div>
+        <button type="submit">Login</button>
+      </form>
     </div>
   );
 }
